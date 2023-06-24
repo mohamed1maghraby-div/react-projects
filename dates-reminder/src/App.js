29eec4a3ae3403fc7ab67fc7fdc32a463fa5e458
mohamed1maghraby-div <mohamed1maghraby@yahoo.com>
2023-06-24 07:59:47 +0300

    finsh Dates-reminder project

--- dates-reminder/src/App.js
+++ dates-reminder/src/App.js
@@ -1,21 +1,24 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import {person} from './data';
+import DatesCount from "./component/DatesCount";
+import DatesList from './component/DatesList';
+import DatesAction from "./component/DatesAction";
+import React, { useState } from "react";
 
 function App() {
+  const [personData, setpersonData] = useState(person)
+  const onDelete=()=>{
+    setpersonData([])
+  }
+  const onViewData=()=>{
+    setpersonData(person)
+  }
   return (
     <div className="font color-body">
       <Container className="py-5">
-        <Row className="justify-content-center">
-          <Col sm="8" className="">
-            لديك 4 مواعيد اليوم
-          </Col>
-        </Row>
-        <Row className="justify-content-center">
-          <Col sm="8" className="">
-            <div className="rectangle">
-
-            </div>
-          </Col>
-        </Row>
+        <DatesCount person={personData}/>
+        <DatesList person={personData}/>
+        <DatesAction deleteData={onDelete} onViewData={onViewData}/>
       </Container>
     </div>
   );
---
+++ dates-reminder/src/data.js
@@ -0,0 +1,7 @@
+export const person =[
+    { id: '1', name: 'احمد', date: '5 مساء', img: 'p1.jpg' },
+    { id: '2', name: 'ممد', date: '4 مساء', img: 'p1.jpg' },
+    { id: '3', name: 'على', date: '3 مساء', img: 'p1.jpg' },
+    { id: '4', name: 'محمود', date: '2 مساء', img: 'p1.jpg' },
+    { id: '5', name: 'حسين', date: '1 مساء', img: 'p1.jpg' }
+  ];
